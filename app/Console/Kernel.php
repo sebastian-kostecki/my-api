d952587adc2c903a0fc8097a853515b779290dda
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-05-19 22:35:08 +0200

    Email every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('email:report-daily-tasks')->hourlyAt('18:00');
+        $schedule->command('email:report-daily-tasks')->everyFiveMinutes();
     }
 
     /**
