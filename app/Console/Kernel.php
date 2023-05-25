55e9a0adf960a4f8030b9fea7d2176e85f1bb04e
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-05-25 14:58:13 +0200

    changed email to 18:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('email:report-daily-tasks')->daily();
+        $schedule->command('email:report-daily-tasks')->dailyAt('18:00');
     }
 
     /**
