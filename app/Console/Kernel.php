3746303748a54e33c877d231b379289e72080988
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-05-25 22:47:50 +0200

    Finished command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('email:report-daily-tasks')->dailyAt('18:00');
+        $schedule->command('email:manage-panel-alpha-mailbox')->daily();
     }
 
     /**
