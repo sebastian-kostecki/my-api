5997be89f7da56ca89c96d317b3744598cb0a58b
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-07-31 23:26:04 +0200

    Created header

--- resources/views/developer.blade.php
+++ resources/views/developer.blade.php
@@ -3,16 +3,46 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Sebastian Kostecki</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&family=Wix+Madefor+Display:wght@400;700;800&display=swap"
+        rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-<h1>Hello, world!</h1>
+<div class="vh-100">
+    <div class="header h-100">
+        <div class="container h-100 position-relative">
+            <div class="w-100 position-absolute top-50 start-50 translate-middle">
+                <h1 class="title">SEBASTIAN KOSTECKI</h1>
+                <h2 class="subtitle">Webdeveloper | PHP | Laravel</h2>
+            </div>
+            <div class="position-absolute bottom-0 start-50 translate-middle-x mb-3">
+                <ul class="list-unstyled d-flex">
+                    <li class="me-2 fs-5">
+                        <a class="link-light link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
+                           href="#">Umiejętności</a>
+                    </li>
+                    <li class="me-2 fs-5">
+                        <a class="link-light link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
+                           href="#">Projekty</a>
+                    </li>
+                    <li class="me-2 fs-5">
+                        <a class="link-light link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
+                           href="#">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h1>Hello, world!</h1>
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
