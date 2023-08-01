b7bfc7ce8a7bb62f7cf496b75f6157e58d764449
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-08-01 14:26:00 +0200

    Footer

--- resources/views/developer.blade.php
+++ resources/views/developer.blade.php
@@ -33,7 +33,7 @@
                     </li>
                     <li class="me-2 fs-5">
                         <a class="link-light link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
-                           href="#">Moje Projekty</a>
+                           href="#projects">Moje Projekty</a>
                     </li>
                     <li class="me-2 fs-5">
                         <a class="link-light link-offset-2 link-underline-opacity-10 link-underline-opacity-100-hover"
@@ -169,13 +169,13 @@
             <div class="container">
                 <div class="bg-black text-white">
                     <div class="container py-5">
-                        <h1 id="skills" class="skills-header pb-4">Moje Projekty</h1>
+                        <h1 id="projects" class="skills-header pb-4">Moje Projekty</h1>
                     </div>
                 </div>
                 <div class="bg-black text-white">
                     <div class="container py-5">
                         <h1 id="contact" class="skills-header text-center">Kontakt ze mną</h1>
-                        <h2 class="text-center fs-5">Chcesz współpracować? To świetnie! Oto moje dane kontaktowe:</h2>
+                        <h2 class="text-center fs-6">Chcesz współpracować? To świetnie! Oto moje dane kontaktowe:</h2>
                     </div>
                     <div class="row">
                         <div
@@ -223,7 +223,6 @@
                                 <div class="rounded-circle bg-secondary contact-card-icon position-relative">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 0 448 512"
                                          class="position-absolute top-50 start-50 translate-middle">
-                                        <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                         <style>svg {
                                                 fill: #ffffff
                                             }</style>
@@ -240,17 +239,63 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bg-black text-white">
-                        <div class="container py-5">
-                            <h1 class="skills-header mb-4">Czekasz na idealnego developera?</h1>
-                            <a class="btn btn-danger me-3" href="#contact" role="button">Skontaktuj się ze mną</a>
-                            <a class="btn btn-danger" href="#skills" role="button">Sprawdź moje umiejętności</a>
-                        </div>
+                    <div class="py-5">
+                        <h1 class="skills-header mb-4">Czekasz na idealnego developera?</h1>
+                        <a class="btn btn-danger me-3" href="#contact" role="button">Skontaktuj się ze mną</a>
+                        <a class="btn btn-danger" href="#skills" role="button">Sprawdź moje umiejętności</a>
                     </div>
                 </div>
             </div>
         </div>
     </main>
+    <footer class="bg-black text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <p>Usługi</p>
+                    <ul class="list-unstyled">
+                        <li>Tworzenie stron i aplikacji internetowych</li>
+                        <li>Back-end dla aplikacji internetowych</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <p>Media</p>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a class="link-light text-decoration-none"
+                               href="https://www.linkedin.com/in/kostecki-sebastian">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a class="link-light text-decoration-none" href="https://github.com/sebastian-kostecki">GitHub</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mt-5">
+                <p>Wszystkie prawa zastrzeżone &copy; 2023</p>
+                <div>
+                    <a class="me-2" href="https://www.linkedin.com/in/kostecki-sebastian">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 448 512">
+                            <style>svg {
+                                    fill: #ffffff
+                                }</style>
+                            <path
+                                d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
+                        </svg>
+                    </a>
+                    <a href="https://github.com/sebastian-kostecki">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 496 512">
+                            <style>svg {
+                                    fill: #ffffff
+                                }</style>
+                            <path
+                                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
