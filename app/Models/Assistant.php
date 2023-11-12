cfabde89bcc26c96dc4844e43bda9efcc1566d43
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-11-12 22:56:54 +0100

    Created assistants model

---
+++ app/Models/Assistant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assistant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'action_id',
+        'assistant_remote_id'
+    ];
+}
