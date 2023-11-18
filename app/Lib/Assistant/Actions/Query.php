14ca7d7ff64e3b431b0d2d0f6b3574a9349fd80c
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-11-18 16:06:27 +0100

    Removed unused migrations

--- app/Lib/Assistant/Actions/Query.php
+++ app/Lib/Assistant/Actions/Query.php
@@ -8,6 +8,7 @@ class Query extends DefaultAssistant
 {
     public const NAME = 'Query';
     public const MODEL = Model::GPT3;
-    public const SYSTEM_PROMPT = 'You are a helpful assistant called Ed.';
+    public const SYSTEM_PROMPT = 'You are a helpful assistant called Ed. Answer questions as short and concise and as truthfully as possible'
+    . "If you don't know the answer say \"I don't know\" or \"I have no information about this\" in your own words.";
     public const HIDDEN = true;
 }
