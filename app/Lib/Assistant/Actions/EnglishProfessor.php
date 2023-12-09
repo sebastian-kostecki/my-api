a211d84a5146608c7af3591ff5894bdccb37ecd8
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2023-12-09 23:13:32 +0100

    Added new actions

---
+++ app/Lib/Assistant/Actions/EnglishProfessor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Lib\Assistant\Actions;
+
+use App\Enums\Assistant\ChatModel as Model;
+
+class EnglishProfessor extends DefaultAssistant
+{
+    public const NAME = 'Boost English';
+    public const ICON = 'fa-solid fa-comment';
+    public const MODEL = Model::GPT3;
+
+    public const INSTRUCTIONS = 'As an English Professor, your task is to review and refine English language usage, focusing on phrases and sentences. ' .
+    'Users will submit text passages, and you are to meticulously analyze them for grammatical accuracy, clarity, coherence, and overall language proficiency. ' .
+    'Provide detailed feedback on areas that require improvement, including suggestions for better word choices, sentence structures, and adherence to English grammar rules. ' .
+    'Correct any errors and offer explanations to help users enhance their writing skills. ' .
+    'Emphasize proper usage of idioms, expressions, and ensure the text maintains a consistent and polished tone.  ' .
+    'Your aim is to elevate the overall quality of written English and assist users in expressing themselves effectively.' .
+    'Feel free to tailor your feedback based on the user\'s proficiency level and provide explanations to aid in their understanding of the language nuances. ' .
+    'Always return only the corrected text without explanations.';
+
+    /**
+     * @var array
+     */
+    public static array $configFields = [
+        'name' => [
+            'name' => 'name',
+            'label' => 'Name',
+            'type' => 'text',
+            'default' => self::NAME
+        ],
+        'icon' => [
+            'name' => 'icon',
+            'label' => 'Icon',
+            'type' => 'text',
+            'default' => self::ICON
+        ],
+        'instructions' => [
+            'name' => 'instructions',
+            'label' => 'Instructions',
+            'type' => 'textarea',
+            'default' => self::INSTRUCTIONS
+        ],
+        'model' => [
+            'name' => 'model',
+            'label' => 'Model',
+            'type' => 'model',
+            'default' => self::MODEL
+        ]
+    ];
+}
---
+++ app/Lib/Assistant/Actions/Refactoring.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Lib\Assistant\Actions;
+
+use App\Enums\Assistant\ChatModel as Model;
+
+class Refactoring extends DefaultAssistant
+{
+    public const NAME = 'Refactor';
+    public const ICON = 'fa-solid fa-file-code';
+    public const SHORTCUT = 'CommandOrControl+Shift+R';
+    public const MODEL = Model::GPT3;
+
+    public const INSTRUCTIONS = 'Your role is to assess whether a given piece of code has undergone proper refactoring. ' .
+    'Users will provide sections of code, and you need to evaluate them for adherence to refactoring principles.' .
+    'Please provide a segment of code in the input, and in the output, present detailed information about the code\'s refactoring status. ' .
+    'Include insights into improvements made in terms of code readability, maintainability, and efficiency. ' .
+    'Additionally, provide a refactored version of the input code, demonstrating how it could be improved. ' .
+    'Identify and comment on instances where the code could benefit from further refactoring or where established best practices have been followed.' .
+    'Offer specific suggestions for improvements, considering factors such as code structure, naming conventions, and the application of design patterns. ' .
+    'Your goal is to guide users towards code that is not only functional but also well-organized and easy to maintain. ' .
+    'Please provide your feedback and suggestions in a constructive manner, highlighting areas of improvement and explaining the rationale behind your recommendations.' .
+    'Feel free to customize this prompt based on specific criteria or principles you want to emphasize during the refactoring assessment.' .
+    'Please provide your responses in Polish. Limit your feedback to the use of single methods for code evaluation, as requested by the user.' .
+    'This way, you will follow the specified language requirements while maintaining the review process in English and delivering responses in Polish.';
+
+    /**
+     * @var array
+     */
+    public static array $configFields = [
+        'name' => [
+            'name' => 'name',
+            'label' => 'Name',
+            'type' => 'text',
+            'default' => self::NAME
+        ],
+        'icon' => [
+            'name' => 'icon',
+            'label' => 'Icon',
+            'type' => 'text',
+            'default' => self::ICON
+        ],
+        'shortcut' => [
+            'name' => 'shortcut',
+            'label' => 'Shortcut',
+            'type' => 'text',
+            'default' => self::SHORTCUT
+        ],
+        'instructions' => [
+            'name' => 'instructions',
+            'label' => 'Instructions',
+            'type' => 'textarea',
+            'default' => self::INSTRUCTIONS
+        ],
+        'model' => [
+            'name' => 'model',
+            'label' => 'Model',
+            'type' => 'model',
+            'default' => self::MODEL
+        ]
+    ];
+}
