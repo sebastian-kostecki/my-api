3f826654be3007d8b0511b2dcde8fbe0ca209fed
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2024-06-14 21:42:00 +0200

    clean web routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Homepage\HomepageController;
-use App\Jobs\TestJob;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,30 +19,10 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get('/', [HomepageController::class, 'index']);
 
-
-Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
-
-Route::post('/login', function (Request $request) {
-    return;
-})->name('login');
-
-
-Route::post('/slack/message', [\App\Http\Controllers\Assistant\AssistantController::class, 'getMessage']);
+//Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
 Route::get('/test', function () {
     $result = "That's fine";
-
-//    $api = new \App\Lib\Apis\Qdrant();
-//    //$result = $api->listCollections();
-//    $result = $api->getCollection('myapi');
-
-    $model = 'text-embedding-3-small';
-    $input = "Something goes wrong";
-    $result = \App\Lib\Apis\OpenAI::factory()->embeddings($model, $input);
-
-
-
-
     dd($result);
 });
 
