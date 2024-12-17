02f47040bff59278cf21f27d560962ade8675c54
Sebastian Kostecki <sebastian.kostecki@gmail.com>
2024-12-17 20:23:09 +0100

    Fix email

--- resources/views/mail/report-daily-tasks.blade.php
+++ resources/views/mail/report-daily-tasks.blade.php
@@ -1,16 +1,16 @@
 <x-mail::message>
 <div style="background-color: #f2f2f2; padding: 15px; margin-bottom: 20px; border-radius: 5px;">
-    <h2 style="margin: 0; font-size: 24px; color: #333; background-color: transparent">Raport: {{$date}}</h2>
+    <h2 style="margin: 0; font-size: 20px; color: #333; background-color: transparent">Raport: {{$date}}</h2>
 </div>
 <div style="background-color: #f2f2f2; padding: 10px; margin-bottom: 10px; border-radius: 5px;">
-    <h3 style="margin: 0; font-size: 20px; color: #333; background-color: transparent">Today</h3>
+    <h3 style="margin: 0; font-size: 16px; color: #333; background-color: transparent">Today</h3>
 </div>
-<table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+<table style="width: 100%; border-collapse: collapse; margin-bottom: 20px; font-size: 14px;">
     <tr>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: left; font-weight: bold;">No</th>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: left; font-weight: bold;">Task</th>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: center; font-weight: bold;">Milestone</th>
-        <th style="background-color: #f2f2f2; padding: 10px; text-align: center; font-weight: bold;">Status</th>
+        <th style="background-color: #f2f2f2; padding: 10px; text-align: right; font-weight: bold;">Status</th>
     </tr>
     @foreach($dailyTasks as $task)
         <tr>
@@ -19,7 +19,7 @@
                 <a href="{{$task['url']}}" target="_blank" style="color: #007bff; text-decoration: none;">{{$task['name']}}</a>
             </td>
             <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">{{ $task['milestone'] }}</td>
-            <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">
+            <td style="padding: 10px; border: 1px solid #ddd; text-align: right;">
   <span style="background-color: {{$task['status']['color']}}; padding: 2px 5px; border-radius: 5px; color: white; box-shadow: 1px 1px 3px rgba(0,0,0,0.1);">
     {{$task['status']['name']}}
   </span>
@@ -28,15 +28,15 @@
     @endforeach
 </table>
 <div style="background-color: #f2f2f2; padding: 10px; margin-bottom: 10px; border-radius: 5px;">
-    <h3 style="margin: 0; font-size: 20px; color: #333; background-color: transparent">Next</h3>
+    <h3 style="margin: 0; font-size: 16px; color: #333; background-color: transparent">Next</h3>
 </div>
-<table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+<table style="width: 100%; border-collapse: collapse; margin-bottom: 20px; font-size: 14px;">
     <tr>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: left; font-weight: bold;">No</th>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: left; font-weight: bold;">Task</th>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: center; font-weight: bold;">Milestone</th>
         <th style="background-color: #f2f2f2; padding: 10px; text-align: center; font-weight: bold;">Priority</th>
-        <th style="background-color: #f2f2f2; padding: 10px; text-align: center; font-weight: bold;">Status</th>
+        <th style="background-color: #f2f2f2; padding: 10px; text-align: right; font-weight: bold;">Status</th>
     </tr>
     @foreach($nextTasks as $task)
         <tr>
@@ -50,7 +50,7 @@
     {{$task['priority']['name']}}
   </span>
             </td>
-            <td style="padding: 10px; border: 1px solid #ddd; text-align: center;">
+            <td style="padding: 10px; border: 1px solid #ddd; text-align: right;">
   <span style="background-color: {{$task['status']['color']}}; padding: 2px 5px; border-radius: 5px; color: white; box-shadow: 1px 1px 3px rgba(0,0,0,0.1);">
     {{$task['status']['name']}}
   </span>
